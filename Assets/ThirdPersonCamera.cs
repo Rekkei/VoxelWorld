6c98dc4fbc5c85439bfe3270ef69f116f58b8cbc
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-06-25 10:26:13 +0300

    hotfix

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform target; 
-    public Vector3 offset = new Vector3(0f, 1.5f, -5f); 
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 1.5f, -5f);
 
     void LateUpdate()
     {
-        
+
         transform.position = target.position + offset;
         transform.LookAt(target.position);
     }
