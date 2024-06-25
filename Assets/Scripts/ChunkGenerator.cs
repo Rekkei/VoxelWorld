c52d90df3cf2a74b7fa9e824dc24d3484d8abf49
Tuna Yavuz <tunayavuz.95@hotmail.com>
2024-06-25 10:51:19 +0300

    PerlinNoise added

--- Assets/Scripts/ChunkGenerator.cs
+++ Assets/Scripts/ChunkGenerator.cs
@@ -5,15 +5,17 @@ public class ChunkData
 {
     public GameObject[] cubePrefabs;
     public int height;
+    public bool isRugged; 
 }
 
 public class ChunkGenerator : MonoBehaviour
 {
     public ChunkData[] chunks;
     public int chunkSize = 20;
-    public float borderThickness = 0.1f; // Thickness of the border walls
-    public GameObject borderPrefab; // Prefab for the border walls
-    public float borderHeight = 100f; // Height of the border walls
+    public float borderThickness = 0.1f;
+    public GameObject borderPrefab; 
+    public float borderHeight = 100f; 
+    public float ruggedness = 0.1f; 
 
     private Transform playerTransform;
 
@@ -40,9 +42,17 @@ public class ChunkGenerator : MonoBehaviour
     {
         for (int x = 0; x < chunkSize; x++)
         {
-            for (int y = 0; y < chunkData.height; y++)
+            for (int z = 0; z < chunkSize; z++)
             {
-                for (int z = 0; z < chunkSize; z++)
+                int height = chunkData.height;
+
+                if (chunkData.isRugged)
+                {
+                    
+                    height = Mathf.RoundToInt(chunkData.height * Mathf.PerlinNoise(x * ruggedness, z * ruggedness));
+                }
+
+                for (int y = 0; y < height; y++)
                 {
                     GameObject cubePrefab = chunkData.cubePrefabs[Random.Range(0, chunkData.cubePrefabs.Length)];
                     Instantiate(cubePrefab, new Vector3(x, y, z) + position, Quaternion.identity);
@@ -62,18 +72,18 @@ public class ChunkGenerator : MonoBehaviour
         float mapWidth = chunkSize;
         float mapHeight = totalHeight;
 
-        // Create borders around the map
+        
         CreateBorder(new Vector3((mapWidth / 2), borderHeight / 2, (-borderThickness / 2) - 1f), new Vector3(mapWidth, borderHeight, borderThickness)); // Front border
-        CreateBorder(new Vector3((mapWidth / 2), borderHeight / 2, (chunkSize - borderThickness / 2)+1f), new Vector3(mapWidth, borderHeight, borderThickness)); // Back border
-        CreateBorder(new Vector3((-borderThickness / 2)-1f, borderHeight / 2, mapWidth / 2), new Vector3(borderThickness, borderHeight, mapWidth)); // Left border
-        CreateBorder(new Vector3((chunkSize - borderThickness / 2)+1f, borderHeight / 2, mapWidth / 2), new Vector3(borderThickness, borderHeight, mapWidth)); // Right border
+        CreateBorder(new Vector3((mapWidth / 2), borderHeight / 2, (chunkSize - borderThickness / 2) + 1f), new Vector3(mapWidth, borderHeight, borderThickness)); // Back border
+        CreateBorder(new Vector3((-borderThickness / 2) - 1f, borderHeight / 2, mapWidth / 2), new Vector3(borderThickness, borderHeight, mapWidth)); // Left border
+        CreateBorder(new Vector3((chunkSize - borderThickness / 2) + 1f, borderHeight / 2, mapWidth / 2), new Vector3(borderThickness, borderHeight, mapWidth)); // Right border
     }
 
     void CreateBorder(Vector3 position, Vector3 scale)
     {
         GameObject border = Instantiate(borderPrefab, position, Quaternion.identity);
         border.transform.localScale = scale;
-        border.transform.SetParent(transform); // Optional: Organize borders under the map generator
+        border.transform.SetParent(transform); 
     }
 
     void Update()
